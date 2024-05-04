723c2eb7acaa0d4b71f13546913058306d19edc7
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-04 19:45:00 -0400

    [REFRACTOR] Fix rendering

--- src/app.py
+++ src/app.py
@@ -1,33 +1,40 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import threading
-from api.alpha_vantage import fetch_continuous_updates
+import time
+from api.alpha_vantage import fetch_stock_data, fetch_continuous_updates
 
 app = Flask(__name__)
 
-# Setting up a global dictionary to hold the latest data
+# This will hold the latest fetched data
 latest_data = {}
 
 def update_data(symbol, interval, api_key, update_interval):
     global latest_data
+    # This loop will continuously update the latest data
     while True:
-        latest_data[symbol] = fetch_continuous_updates(symbol, interval, api_key, update_interval)
+        data = fetch_stock_data(symbol, interval, api_key)
+        if isinstance(data, dict):
+            latest_data[symbol] = data  # Store the latest data for the symbol
+        time.sleep(update_interval)
 
 @app.route('/')
 def home():
-    # Assuming you want to display data for 'IBM'
     symbol = 'IBM'
-    if symbol in latest_data:
-        return render_template('index.html', stock_data=latest_data[symbol])
+    data = latest_data.get(symbol, None)
+    if data:
+        # Extracting the latest timestamp's data
+        latest_point = next(iter(data['Time Series (5min)'].values()))
+        return render_template('index.html', stock_data=latest_point)
     else:
-        return "Waiting for data..."
+        return render_template('index.html', stock_data=None)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     api_key = 'GZY5P76URW9V54GH'
     symbol = 'IBM'
     interval = '5min'
-    update_interval = 300
+    update_interval = 300  # Update every 5 minutes
 
-    # Start a background thread to update stock data continuously
+    # Starting a background thread to fetch data continuously
     thread = threading.Thread(target=update_data, args=(symbol, interval, api_key, update_interval))
     thread.daemon = True
     thread.start()
