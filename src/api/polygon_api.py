fdba51e7fdd6e4958398f7739c7de96ee96e580a
zikrya <77081809+zikrya@users.noreply.github.com>
2024-05-05 16:16:17 -0400

    [FEAT] Better caching mechanism

--- src/api/polygon_api.py
+++ src/api/polygon_api.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 def fetch_stock_data(symbol, api_key):
-    url = f'https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey={api_key}'
+    url = f"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey={api_key}"
     try:
         response = requests.get(url)
         if response.status_code == 200:
--- src/app.py
+++ src/app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, jsonify
+from flask_caching import Cache
 import threading
 import time
 from api.polygon_api import fetch_stock_data
 
 app = Flask(__name__)
+app.config['CACHE_TYPE'] = 'simple'
+cache = Cache(app)
+cache.init_app(app)
 
 symbols = ['AAPL', 'IBM']
 latest_data = {}
 
 @app.route('/api/data')
+@cache.cached(timeout=300, key_prefix='stock_data')
 def api_data():
     return jsonify(latest_data)
 
@@ -33,4 +38,3 @@ if __name__ == '__main__':
     thread.daemon = True
     thread.start()
     app.run(debug=True)
-
